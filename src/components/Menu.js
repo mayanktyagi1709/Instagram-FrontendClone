e4d0cf6288f059ca7da786ae5f3493f0601085ca
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-19 16:39:06 +0530

    Created ProfileIcon component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,6 +5,8 @@ import {ReactComponent as Home} from "../images/home.svg"
 import {ReactComponent as Inbox} from "../images/inbox.svg"
 import {ReactComponent as Explore} from "../images/explore.svg"
 import {ReactComponent as Notifications} from "../images/notifications.svg"
+import image from "../images/profile.jpg"
+import ProfileIcon from "./ProfileIcon";
 
 const Menu = () => {
   return (
@@ -13,6 +15,7 @@ const Menu = () => {
         <Inbox className="icon"/>
         <Explore className="icon"/>
         <Notifications className="icon"/>
+        <ProfileIcon iconSize="small" image={image} />
       </div>
   );
 };
