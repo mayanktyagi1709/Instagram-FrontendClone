2460415894800ebdebb2753f481e17864c20835c
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-20 10:19:42 +0530

    Created cards component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import "../styles/App.scss";
+import Cards from "./Cards";
 import Navigation from "./Navigation";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Cards/>
+      <Sidebar/>
     </>
   );
 }
---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/cards.scss";
+import Stories from "./Stories";
+
+const Cards = () => {
+  return (
+    <div className="cards">
+      <Stories />
+    </div>
+  );
+};
+
+export default Cards;
