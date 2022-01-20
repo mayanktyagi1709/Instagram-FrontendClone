8f02696f506b40bd33857d7898e3abedc71faf36
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-20 12:23:26 +0530

    Added posts on the page

---
+++ src/components/Comment.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/comment.scss";
+
+const Comment = (props) => {
+  const { accountName, comment } = props;
+
+  return (
+    <div className="commentContainer">
+      <div className="accountName">{accountName}</div>
+      <div className="comment">{comment}</div>
+    </div>
+  );
+};
+
+export default Comment;
