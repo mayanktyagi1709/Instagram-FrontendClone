8b0659a50c222252a8a1a2be563fcac942f6355b
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2022-01-20 10:14:41 +0530

    Displaying stories on the app

---
+++ src/components/Stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/stories.scss";
+import HorizontalScroll from "react-scroll-horizontal";
+import Story from "./Story";
+
+const Stories = () => {
+  return (
+    <div className="stories">
+      <HorizontalScroll className="scroll" reverseScroll={true}>
+        <Story/>
+      </HorizontalScroll>
+    </div>
+  );
+};
+
+export default Stories;
